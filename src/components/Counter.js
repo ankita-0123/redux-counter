50b17c77a151223bd6290c2945e26ec15f826d72
ankita-0123 <pragyanparamita1449@gmail.com>
2023-03-29 19:35:15 +0530

    commit id

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';
+
+const Counter = () => {
+    const counter = useSelector(state=>state.counter);
+    const toggleCounterHandler = () => {}
+  return (
+    <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{counter}</div>
+        <button  onClick={toggleCounterHandler}>Toggle counter</button>
+    </main>
+  )
+}
+
+export default Counter
